62a063b1e250e15d91b058eb388acf351795187b
stanleylexander <stanley.6ho@gmail.com>
2025-06-01 15:57:15 +0700

    first commit

---
+++ app/src/main/java/com/week1/healing_yuk/SignUpActivity.kt
@@ -0,0 +1,18 @@
+package com.week1.healing_yuk
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.week1.healing_yuk.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
