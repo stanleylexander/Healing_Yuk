f5d4a7062ecb696210b33afc8f11ba6726532440
MatCliff <s160422025@student.ubaya.ac.id>
2025-06-02 21:41:24 +0700

    Yg sudah: UI login & signup, udh nambah botomNav+Fragment(Explore,Favourite,Profil) utk main

--- app/src/main/java/com/week1/healing_yuk/MainActivity.kt
+++ app/src/main/java/com/week1/healing_yuk/MainActivity.kt
@@ -5,14 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.week1.healing_yuk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    val fragments:ArrayList<Fragment> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fragments.add(ExploreFragment())
+        fragments.add(FavouriteFragment())
+        fragments.add(ProfileFragment())
+        binding.viewPager.adapter = MainAdapter(this,fragments)
+
+        binding.viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                binding.bottomNav.selectedItemId = binding.bottomNav.menu.getItem(position).itemId
+                // Or: binding.bottomNav.selectedItemId = binding.bottomNav.menu[position].itemId
+
+            }
+        })
+        binding.bottomNav.setOnItemSelectedListener {
+            binding.viewPager.currentItem = when(it.itemId) {
+                R.id.ItemExplore -> -0
+                R.id.ItemFavourite -> 1
+                R.id.ItemProfile -> 2
+                else -> 0 // default to home
+            }
+            true
+        }
     }
 }
